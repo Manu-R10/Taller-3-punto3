ae24931947486c5d25574575dc1e8e3c1038441a
taller3 punto 3 <manuelraigosa09@gmail.com>
2025-05-24 22:26:04 -0500

    operacion para prestamo,alert y while

--- punto3.js
+++ punto3.js
@@ -18,4 +18,13 @@ do {
         default:
             alert("Usuario no reconocido :o");
         }
+    prestamo_solicitado = parseInt(prompt("Ingrese la cantidad de libros que desea pedir prestado:"), 10);
+    if (prestamo_solicitado > libros_limite) {
+        alert("Advertencia, el usuario paso el limite permitido :(");
+        }
+    else {
+        alert("Prestamo aprobado :)");
+        }
+    alert("Sistema de prestamo finalizado. Puedes ingresar otro usuario.");
     }
+ while (true); 
